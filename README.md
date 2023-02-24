e4d89417d29abbf73e81bc25d9ac51c2381735fa
Robert Strzelczyk <robertstrzelczyk98@gmail.com>
2023-02-24 16:16:24 +0100

    commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Frontend Mentor - Time tracking dashboard solution
+
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+![](images/final.jpg)
+
+## The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Switch between viewing Daily, Weekly, and Monthly stats
+
+## Links
+
+- Live Site URL: [Dashboard](https://your-live-site-url.com)
+
+## Built with
+
+- HTML5
+- CSS3
+- JavaScript
---
+++ script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const [...arr] = document.querySelectorAll(".stats");
+const menu = document.querySelector(".menu");
+
+window.addEventListener("load", update());
+
+menu.addEventListener("click", (e) => button(e));
+
+async function displayStats(index, timePeriod) {
+  const work = await getData(index);
+  const current = work.timeframes[`${timePeriod}`].current;
+  const previous = work.timeframes[`${timePeriod}`].previous;
+  const fieldCurrent = arr[index].children[0];
+  const fieldPreviou = arr[index].children[1];
+  fieldCurrent.textContent = `${current}hrs`;
+  switch (timePeriod) {
+    case "daily":
+      fieldPreviou.textContent = `Yesterday - ${previous}hrs`;
+      break;
+    case "weekly":
+      fieldPreviou.textContent = `Last Week - ${previous}hrs`;
+      break;
+    case "monthly":
+      fieldPreviou.textContent = `Last Month - ${previous}hrs`;
+      break;
+  }
+}
+
+async function getData(index) {
+  const data = await fetch("data.json");
+  const dataJSON = await data.json();
+  return dataJSON[index];
+}
+
+function update(value = "weekly") {
+  arr.forEach((_, index) => displayStats(index, value));
+}
+
+function button(event) {
+  if (!event.target.classList.contains("button")) return;
+  [...menu.children].forEach((item) =>
+    item.children[0].classList.remove("active")
+  );
+  const currentElement = event.target;
+  currentElement.classList.add("active");
+  update(currentElement.textContent.toLowerCase());
+}
